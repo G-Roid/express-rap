cb4dd7e2551babc486248cfd116b6272ca7bddbe
Geoffroy Guillotte <geoffroy.guillotte@gmail.com>
2022-06-11 18:53:04 +1000

    added cor

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(cor())
 
 app.get('/', (request, response) => {
     console.log('server running.....')
