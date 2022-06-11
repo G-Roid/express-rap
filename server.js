929622ef24f85fa6ae2c964e7f5a952331866e56
Geoffroy Guillotte <geoffroy.guillotte@gmail.com>
2022-06-11 20:31:51 +1000

    added public folder

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const PORT = 8000
-const cors = require('cors')
+// const cors = require('cors')
 
-app.use(cors())
+// app.use(cors())
+
+app.use(express.static('public'))
 
 app.get('/', (request, response) => {
     console.log('server running.....')
